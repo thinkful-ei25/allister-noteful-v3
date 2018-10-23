d4cc3425a21c3b45abcc87cd71faea3decb4a016
Allister Rooke <allisterg@gmail.com>
2018-10-23 15:46:25 -0400

    added regexp to content field and updated server.js

--- scratch/queries.js
+++ scratch/queries.js
@@ -13,7 +13,7 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
         const re = new RegExp(searchTerm, 'i')
 
         if (searchTerm) {
-            filter = { $or: [{ title : re }, { content : re }] }
+            filter = { $or: [{ title: re }, { content: re }] }
         }
 
         return Note.find(filter).sort({ updatedAt: 'desc' });
@@ -29,73 +29,73 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
         console.error(err);
     });
 
-// findById
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-//     .then(() => {
-//         const id = '000000000000000000000004'
-//         return Note.findById(id).sort({ updatedAt: 'desc' });
-//     })
-//     .then(results => {
-//         console.log(results);
-//     })
-//     .then(() => {
-//         return mongoose.disconnect()
-//     })
-//     .catch(err => {
-//         console.error(`ERROR: ${err.message}`);
-//         console.error(err);
-//     });
+findById
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        const id = '000000000000000000000004'
+        return Note.findById(id).sort({ updatedAt: 'desc' });
+    })
+    .then(results => {
+        console.log(results);
+    })
+    .then(() => {
+        return mongoose.disconnect()
+    })
+    .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error(err);
+    });
 
-// //create
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-// .then(() => {
-//     const newNote = {"title" : "5 things about coffee", "content": "Coffee Coffee Coffee Coffee Coffee"}
-//     return Note.create(newNote);
-// })
-// .then(results => {
-//     console.log(results);
-// })
-// .then(() => {
-//     return mongoose.disconnect()
-// })
-// .catch(err => {
-//     console.error(`ERROR: ${err.message}`);
-//     console.error(err);
-// });
+//create
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        const newNote = { "title": "5 things about coffee", "content": "Coffee Coffee Coffee Coffee Coffee" }
+        return Note.create(newNote);
+    })
+    .then(results => {
+        console.log(results);
+    })
+    .then(() => {
+        return mongoose.disconnect()
+    })
+    .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error(err);
+    });
 
-// //update
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-// .then(() => {
-//     const updateObj = {"title" : "I have updated this note", "content" : "This note has been updated"}
-//     const id = '000000000000000000000003'
-//     return Note.findByIdAndUpdate(id, updateObj, {new: true})
-//     .then(results => {
-//         console.log(results);
-//     })
-//     .then(() => {
-//         return mongoose.disconnect()
-//     })
-//     .catch(err => {
-//         console.error(`ERROR: ${err.message}`);
-//         console.error(err);
-//     });
-// })
+//update
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        const updateObj = { "title": "I have updated this note", "content": "This note has been updated" }
+        const id = '000000000000000000000003'
+        return Note.findByIdAndUpdate(id, updateObj, { new: true })
+            .then(results => {
+                console.log(results);
+            })
+            .then(() => {
+                return mongoose.disconnect()
+            })
+            .catch(err => {
+                console.error(`ERROR: ${err.message}`);
+                console.error(err);
+            });
+    })
 
-// //delete
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-//     .then(() => {
-//         const id = '000000000000000000000004'
-//         return Note.findByIdAndRemove(id);
-//     })
-//     .then(results => {
-//         console.log(results);
-//     })
-//     .then(() => {
-//         return mongoose.disconnect()
-//     })
-//     .catch(err => {
-//         console.error(`ERROR: ${err.message}`);
-//         console.error(err);
-//     });
+//delete
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        const id = '000000000000000000000004'
+        return Note.findByIdAndRemove(id);
+    })
+    .then(results => {
+        console.log(results);
+    })
+    .then(() => {
+        return mongoose.disconnect()
+    })
+    .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error(err);
+    });
 
 
